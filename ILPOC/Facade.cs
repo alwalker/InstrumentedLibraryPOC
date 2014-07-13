9e5cef74a94c2818005c9d9af5b082bc0cbdc5d5
Andrew Walker <alwalker21@gmail.com>
2014-07-12 21:09:01 -0500

    Added facade.

---
+++ ILPOC/Facade.cs
@@ -0,0 +1,19 @@
+﻿using ILPOC.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILPOC
+{
+    public class Facade
+    {
+        private static Controller _controller = new Controller();
+
+        public static IList<EntityOne> GetOnesForTwo(string twoName)
+        {
+            return _controller.GetOnesForTwo(twoName);
+        }
+    }
+}
---
+++ ILPOC/Web/MainModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ILPOC.Web
+{
+    public class MainModule: Nancy.NancyModule
+    {
+        public MainModule()
+        {
+            Get["/GetOnesForTwo/{name}"] = _ => { return 200; };
+        }
+    }
+}
